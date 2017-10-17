48c8c2b2b51c227024fcad1ddee68335d6cf25f9
Julianne Johnson <tweetypie417@yahoo.com>
2017-10-17 12:14:04 -0500

    moved map container to separate file

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,45 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AmCharts from '@amcharts/amcharts3-react';
+import Map from './containers/Map.jsx';
 
 class App extends React.Component {
   render () {
     return (
-      <AmCharts.React
-        style={{
-          width: '100%',
-          height: '500px'
-        }}
-        options ={{
-          'type': 'map',
-          'dataProvider' : {
-            'map' : 'worldLow',
-            'areas' : [
-              { 
-                'id' : 'CA',
-                'groupId' : 'North America',
-                'color' : '#67b7dc',
-              }, { 
-                'id' : 'US',
-                'groupId' : 'North America',
-                'color' : '#67b7dc',
-              }, { 
-                'id' : 'MX',
-                'groupId' : 'North America',
-                'color' : '#67b7dc',
-              }, { 
-                'id' : 'AG',
-                'groupId' : 'North America',
-                'color' : '#67b7dc',
-              },
-            ]
-            // 'getAreasFromMap' : true
-          },
-          'areasSettings': {
-            'autoZoom' : true,
-            'selectedColor' : '#CC0000'
-          }
-        }} 
+      <Map
       />
     );
   }

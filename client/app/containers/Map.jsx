261d498cf3795cb790158bde647abf5530477c86
luig0 <jhcao@live.com>
2017-10-17 15:08:40 -0500

    placed all continents

--- client/app/containers/Map.jsx
+++ client/app/containers/Map.jsx
@@ -6,9 +6,25 @@ const geoCenters = {
     'latitude' : 48.3548,
     'longitude' : -99.9989,
   },
+  'SA' : {
+    'latitude' : -15.6006,
+    'longitude' : -56.1004
+  },
+  'EUROPE' : {
+    'latitude' : 54.5423,
+    'longitude' : 25.198
+  },
+  'AFRICA' : {
+    'latitude' : 1.261,
+    'longitude' : 25
+  },
   'ASIA' : {
-    'latitude' : 43.6841,
-    'longitude' : 87.3315
+    'latitude' : 50,
+    'longitude' : 100
+  },
+  'AUSTRALIA' : {
+    'latitude' : -23.02,
+    'longitude' : 140.10
   }
 }
 
@@ -36,7 +52,7 @@ const scale = {
   'minor' : 2.5
 }
 
-const colors = {
+const bubbleColor = {
   'major' : '#00cc62',
   'minor' : 'blue'
 }
@@ -59,7 +75,7 @@ class Map extends React.Component {
                 'latitude' : geoCenters.NA.latitude,
                 'longitude' : geoCenters.NA.longitude,
                 'type' : 'circle',
-                'color' : colors.major,
+                'color' : bubbleColor.major,
                 'scale' : scale.major,
                 'label' : '#TRUMP',
                 'labelPosition' : 'middle',
@@ -67,31 +83,127 @@ class Map extends React.Component {
                 'latitude' : geoCenters.NA.latitude + coordOffsets.north.latitude,
                 'longitude' : geoCenters.NA.longitude + coordOffsets.north.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.NA.latitude + coordOffsets.south.latitude,
                 'longitude' : geoCenters.NA.longitude + coordOffsets.south.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.NA.latitude + coordOffsets.east.latitude,
                 'longitude' : geoCenters.NA.longitude + coordOffsets.east.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.NA.latitude + coordOffsets.west.latitude,
                 'longitude' : geoCenters.NA.longitude + coordOffsets.west.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.SA.latitude,
+                'longitude' : geoCenters.SA.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.major,
+                'scale' : scale.major,
+                'label' : '#TEMER',
+                'labelPosition' : 'middle',
+              }, {
+                'latitude' : geoCenters.SA.latitude + coordOffsets.north.latitude,
+                'longitude' : geoCenters.SA.longitude + coordOffsets.north.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.SA.latitude + coordOffsets.south.latitude,
+                'longitude' : geoCenters.SA.longitude + coordOffsets.south.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.SA.latitude + coordOffsets.east.latitude,
+                'longitude' : geoCenters.SA.longitude + coordOffsets.east.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.SA.latitude + coordOffsets.west.latitude,
+                'longitude' : geoCenters.SA.longitude + coordOffsets.west.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.EUROPE.latitude,
+                'longitude' : geoCenters.EUROPE.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.major,
+                'scale' : scale.major,
+                'label' : '#DRAGHI',
+                'labelPosition' : 'middle',
+              }, {
+                'latitude' : geoCenters.EUROPE.latitude + coordOffsets.north.latitude,
+                'longitude' : geoCenters.EUROPE.longitude + coordOffsets.north.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.EUROPE.latitude + coordOffsets.south.latitude,
+                'longitude' : geoCenters.EUROPE.longitude + coordOffsets.south.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.EUROPE.latitude + coordOffsets.east.latitude,
+                'longitude' : geoCenters.EUROPE.longitude + coordOffsets.east.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.EUROPE.latitude + coordOffsets.west.latitude,
+                'longitude' : geoCenters.EUROPE.longitude + coordOffsets.west.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AFRICA.latitude,
+                'longitude' : geoCenters.AFRICA.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.major,
+                'scale' : scale.major,
+                'label' : '#OshiAgabi',
+                'labelPosition' : 'middle',
+              }, {
+                'latitude' : geoCenters.AFRICA.latitude + coordOffsets.north.latitude,
+                'longitude' : geoCenters.AFRICA.longitude + coordOffsets.north.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AFRICA.latitude + coordOffsets.south.latitude,
+                'longitude' : geoCenters.AFRICA.longitude + coordOffsets.south.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AFRICA.latitude + coordOffsets.east.latitude,
+                'longitude' : geoCenters.AFRICA.longitude + coordOffsets.east.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AFRICA.latitude + coordOffsets.west.latitude,
+                'longitude' : geoCenters.AFRICA.longitude + coordOffsets.west.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.ASIA.latitude,
                 'longitude' : geoCenters.ASIA.longitude,
                 'type' : 'circle',
-                'color' : colors.major,
+                'color' : bubbleColor.major,
                 'scale' : scale.major,
                 'label' : '#PUTIN',
                 'labelPosition' : 'middle',
@@ -99,25 +211,57 @@ class Map extends React.Component {
                 'latitude' : geoCenters.ASIA.latitude + coordOffsets.north.latitude,
                 'longitude' : geoCenters.ASIA.longitude + coordOffsets.north.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.ASIA.latitude + coordOffsets.south.latitude,
                 'longitude' : geoCenters.ASIA.longitude + coordOffsets.south.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.ASIA.latitude + coordOffsets.east.latitude,
                 'longitude' : geoCenters.ASIA.longitude + coordOffsets.east.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               }, {
                 'latitude' : geoCenters.ASIA.latitude + coordOffsets.west.latitude,
                 'longitude' : geoCenters.ASIA.longitude + coordOffsets.west.longitude,
                 'type' : 'circle',
-                'color' : colors.minor,
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AUSTRALIA.latitude,
+                'longitude' : geoCenters.AUSTRALIA.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.major,
+                'scale' : scale.major,
+                'label' : '#OZ',
+                'labelPosition' : 'middle',
+              }, {
+                'latitude' : geoCenters.AUSTRALIA.latitude + coordOffsets.north.latitude,
+                'longitude' : geoCenters.AUSTRALIA.longitude + coordOffsets.north.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AUSTRALIA.latitude + coordOffsets.south.latitude,
+                'longitude' : geoCenters.AUSTRALIA.longitude + coordOffsets.south.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AUSTRALIA.latitude + coordOffsets.east.latitude,
+                'longitude' : geoCenters.AUSTRALIA.longitude + coordOffsets.east.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
+                'scale' : scale.minor,
+              }, {
+                'latitude' : geoCenters.AUSTRALIA.latitude + coordOffsets.west.latitude,
+                'longitude' : geoCenters.AUSTRALIA.longitude + coordOffsets.west.longitude,
+                'type' : 'circle',
+                'color' : bubbleColor.minor,
                 'scale' : scale.minor,
               },
             ],

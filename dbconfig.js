49e0fc95d78869ff1345c0da09dce9aed4dcc83c
Kenneth Kang <kjkang11@gmail.com>
2017-10-19 17:20:18 -0500

    cassandra query execution

--- dbconfig.js
+++ dbconfig.js
@@ -1,52 +1,86 @@
 const Cassandra = require('cassandra-driver')
+const Promise = require('bluebird');
 
 var client = new Cassandra.Client({contactPoints: ['127.0.0.1']})
+
 client.connect(function(err, result) {
 	console.log('Cassandra connected')
 });
 
-`CREATE KEYSPACE new_news_keyspace WITH replication =
-	{'class': 'SimpleStrategy', 'replication_factor': 1};`
-`USE new_news_keyspace;`
-var query = `CREATE TABLE [ IF NOT EXISTS ] articles (
-  // id text PRIMARY KEY, // *primary key
-  // score int,
-  // title text,
-  // country text, // *primary key
-  // crawl_date text,
-  // url text,
-  // host text,
-  // text text,
-  // main_image_url text,
-  // sentiment float,
-  // concepts map<text, float>, // text, relevance *primary key
-	// PRIMARY KEY (concepts )
-)`
-var concept_query = concepts.reduce((string, {text, relevance})=>{
-  return string + ` '${text}' : ${relevance},`;
-}, '{')
-query = query.slice(0,-1) + '}' // `{ 'United States' : 0.983999, 'Puerto Rico' : 0.848328, 'Latin America' : 0.670274, 'Liberal democracies' : 0.421739, 'Pacific Ocean' : 0.417309}`;
-`INSERT INTO articles(id, score, title, country, crawl_date, url,
-	host, text, main_image_url, sentiment, concepts) VALUES (
-		${id}, ${score}, ${title}, ${country}, ${crawl_date}, ${url}, ${host},
-		${text}, ${main_image_url}, ${enriched_text.sentiment.document.score},
-		${concept_query}
-);`
+// CASSANDRA SETUP PROCESS
+/*
+	cqlsh
 
-// // // // // // // // // // // // // // // // // // // // // // // // // //
-// CREATE TABLE loads (
-//     machine inet,
-//     cpu int,
-//     mtime timeuuid,
-//     load float,
-//     PRIMARY KEY ((machine, cpu), mtime)
-// ) WITH CLUSTERING ORDER BY (mtime DESC);
-// // // // // // // // // // // // // // // // // // // // // // // // // //
+	CREATE_KEYSPACE new_news WITH replication =
+	{'class': 'SimpleStrategy', 'replication_factor': 1};
 
-// INSERT INTO people.subscribers
+	USE new_news;
 
-// client.execute(query).then( () => {
-//
-// }).catch( () => {
-//
-// })
+	CREATE TABLE [ IF NOT EXISTS ] articles (
+	  id text,
+		main_concept text,
+	  score int,
+	  title text,
+	  country text,
+	  crawl_date text,
+	  url text,
+	  host text,
+	  text text,
+	  main_image_url text,
+	  sentiment float,
+	  concepts map<text, float>,
+		PRIMARY KEY (id, main_concept, crawl_date, country)
+	) WITH CLUSTERING ORDER BY (crawl_date DESC);
+
+	exit
+*/
+
+// var article_aggregate = [];
+var staged_execution = [];
+
+const WatsonTop10 = require('./watson/top_10_filtered.js'); //STATIC TEST DATA
+WatsonTop10 = WatsonTop10.aggregations.aggregations.results
+
+WatsonTop10.forEach( ({key, aggregations}) => {
+	var articles = aggregations.hits.hits.map( ({id, score, title, country,
+		crawl_date, url, host, text,	main_image_url, enriched_text}) => {
+
+		let concept_query = concepts.reduce((string, {text, relevance})=>{
+		  return string + ` '${text}' : ${relevance},`;
+		}, '{');
+		concept_query = query.slice(0,-1) + '}'
+		// concept_query example: `{ 'United States' : 0.983999, 'Puerto Rico' : 0.848328, ...}`;
+
+		let query = `INSERT INTO articles(id, main_concept, score, title, country,
+			crawl_date, url, host, text, main_image_url, sentiment, concepts)
+			VALUES ( ${id}, ${key}, ${score}, ${title}, ${country}, ${crawl_date},
+			${url}, ${host}, ${text}, ${main_image_url},
+			${enriched_text.sentiment.document.score}, ${concept_query});`
+
+		staged_execution.push(client.execute(query));
+
+		// return  {
+		// 	id: id,
+		// 	main_concept: key,
+		// 	score: score,
+		// 	title: title,
+		// 	country: country,
+		// 	crawl_date: crawl_date,
+		// 	url: url,
+		// 	host: host,
+		// 	text: text,
+		// 	main_image_url: main_image_url,
+		// 	sentiment: enriched_text.sentiment.document.score,
+		// 	concepts: enriched_text.concepts.map(({text, relevance})=>{
+		// 		return {text: text, relevance: relevance};
+		// 	})
+		// };
+	});
+	// article_aggregate = article_aggregate.concat(articles);
+});
+
+Promise.all(staged_execution).then(
+	console.log(`STAGED EXECUTION COMPLETE: ${staged_execution.length} articles successfully inserted into articles table`);
+).catch(
+	console.log(`STAGED EXECUTION FAIL TO RESOLVE`);
+)

4d55c45e46e0e3e6e851eac58a5b9e0b9dc2ca76
Kenneth Kang <kjkang11@gmail.com>
2017-10-20 14:10:38 -0500

    dbconfig minor debugging

--- dbconfig.js
+++ dbconfig.js
@@ -11,26 +11,27 @@ client.connect(function(err, result) {
 /*
 	cqlsh
 
-	CREATE_KEYSPACE new_news WITH replication =
+	CREATE KEYSPACE new_news WITH replication =
 	{'class': 'SimpleStrategy', 'replication_factor': 1};
 
 	USE new_news;
 
-	CREATE TABLE [ IF NOT EXISTS ] articles (
-	  id text,
+
+	CREATE TABLE articles (
+		id text,
 		main_concept text,
-	  score int,
-	  title text,
-	  country text,
-	  crawl_date text,
-	  url text,
-	  host text,
-	  text text,
-	  main_image_url text,
-	  sentiment float,
-	  concepts map<text, float>,
-		PRIMARY KEY (id, main_concept, crawl_date, country)
-	) WITH CLUSTERING ORDER BY (crawl_date DESC);
+		score int,
+		title text,
+		country text,
+		crawl_date text,
+		url text,
+		host text,
+		text text,
+		main_image_url text,
+		sentiment float,
+		concepts map<text, float>,
+		PRIMARY KEY (id, crawl_date, main_concept, country)
+	) WITH CLUSTERING ORDER BY (crawl_date DESC, main_concept ASC, country ASC);
 
 	exit
 */
@@ -38,17 +39,17 @@ client.connect(function(err, result) {
 // var article_aggregate = [];
 var staged_execution = [];
 
-const WatsonTop10 = require('./watson/top_10_filtered.js'); //STATIC TEST DATA
-WatsonTop10 = WatsonTop10.aggregations.aggregations.results
+var WatsonTop10 = require('./watson/top_10_filtered.js'); //STATIC TEST DATA
+WatsonTop10 = WatsonTop10.aggregations[0].aggregations[0].results
 
 WatsonTop10.forEach( ({key, aggregations}) => {
-	var articles = aggregations.hits.hits.map( ({id, score, title, country,
+	var articles = aggregations[0].hits.hits.map( ({id, score, title, country,
 		crawl_date, url, host, text,	main_image_url, enriched_text}) => {
 
-		let concept_query = concepts.reduce((string, {text, relevance})=>{
+		let concept_query = enriched_text.concepts.reduce((string, {text, relevance})=>{
 		  return string + ` '${text}' : ${relevance},`;
 		}, '{');
-		concept_query = query.slice(0,-1) + '}'
+		concept_query = concept_query.slice(0,-1) + '}'
 		// concept_query example: `{ 'United States' : 0.983999, 'Puerto Rico' : 0.848328, ...}`;
 
 		let query = `INSERT INTO articles(id, main_concept, score, title, country,
@@ -57,6 +58,7 @@ WatsonTop10.forEach( ({key, aggregations}) => {
 			${url}, ${host}, ${text}, ${main_image_url},
 			${enriched_text.sentiment.document.score}, ${concept_query});`
 
+		console.log(query)
 		staged_execution.push(client.execute(query));
 
 		// return  {
@@ -79,8 +81,9 @@ WatsonTop10.forEach( ({key, aggregations}) => {
 	// article_aggregate = article_aggregate.concat(articles);
 });
 
-Promise.all(staged_execution).then(
+Promise.all(staged_execution).then( () => {
 	console.log(`STAGED EXECUTION COMPLETE: ${staged_execution.length} articles successfully inserted into articles table`);
-).catch(
+
+}).catch( () => {
 	console.log(`STAGED EXECUTION FAIL TO RESOLVE`);
-)
+})

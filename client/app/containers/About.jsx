c9703a2c95ee510eb2782f69b1d4e84beba0063f
Peter Choi <phc284@gmail.com>
2017-10-27 17:24:14 -0500

    uninstalled wrong package (FIXED)

--- client/app/containers/About.jsx
+++ client/app/containers/About.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from "material-ui/RaisedButton";
 
 const styles = {
   button: {
     padding: 1,
-    borderStyle: 'solid',
-    borderWidth: 2,
+    borderStyle: "solid",
+    borderWidth: 2
   }
-}
+};
 
 const About = () => (
   <div className="aboutpage">
@@ -16,7 +16,7 @@ const About = () => (
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-12">
-            <h1 className="brand-heading">Meet the Team</h1>
+            <h1 className="brand-heading">Meet our Team</h1>
           </div>
         </div>
       </div>
@@ -24,12 +24,24 @@ const About = () => (
 
     <section id="description">
       <div className="container">
-          <div>
-            <h1>We are dotConnection</h1>
-            <h4>We are bringing blah blah blah blah blah blah blah blah blah blah blah blah blah blah</h4>
-            <span><RaisedButton buttonStyle ={{padding: 5}}style={styles.button}><a target="_blank" href="https://github.com/dotConnection/newnews"> View Our Github </a></RaisedButton></span>
-         </div>
+        <div>
+          <h1>We are dotConnection</h1>
+          <h4>
+            We are bringing blah blah blah blah blah blah blah blah blah blah
+            blah blah blah blah
+          </h4>
+          <span>
+            <RaisedButton buttonStyle={{ padding: 5 }} style={styles.button}>
+              <a
+                target="_blank"
+                href="https://github.com/dotConnection/newnews"
+              >
+                View Our Github
+              </a>
+            </RaisedButton>
+          </span>
         </div>
+      </div>
     </section>
 
     <section id="julie">
@@ -41,7 +53,12 @@ const About = () => (
           />
           <div>
             <h1>Julie Johnson</h1>
-            <h4>Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something </h4>
+            <h4>
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something
+            </h4>
           </div>
         </div>
       </div>
@@ -52,11 +69,16 @@ const About = () => (
         <div className="row">
           <div>
             <h1>Jonathan Cao</h1>
-            <h4>Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something </h4>
+            <h4>
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something
+            </h4>
           </div>
           <img
-          className="profpic"
-          src="https://vignette1.wikia.nocookie.net/disney/images/5/54/Maui.jpg/revision/latest?cb=20161226102355"
+            className="profpic"
+            src="https://vignette1.wikia.nocookie.net/disney/images/5/54/Maui.jpg/revision/latest?cb=20161226102355"
           />
         </div>
       </div>
@@ -71,8 +93,12 @@ const About = () => (
           />
           <div>
             <h1>Kenneth Kang</h1>
-            <h4>Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something </h4>
-
+            <h4>
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something
+            </h4>
           </div>
         </div>
       </div>
@@ -83,12 +109,16 @@ const About = () => (
         <div className="row">
           <div>
             <h1>Peter Choi</h1>
-            <h4>Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something Something </h4>
-
+            <h4>
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something Something Something Something Something
+              Something Something d
+            </h4>
           </div>
           <img
-          className="profpic"
-          src="https://vignette1.wikia.nocookie.net/disney/images/5/54/Maui.jpg/revision/latest?cb=20161226102355"
+            className="profpic"
+            src="https://vignette1.wikia.nocookie.net/disney/images/5/54/Maui.jpg/revision/latest?cb=20161226102355"
           />
         </div>
       </div>
--- server-config.js
+++ server-config.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const serve = require('koa-static');
-const sendfile = require('koa-sendfile')
+const send = require('koa-send')
 
 const dbFetch = require('./requestHandlers/dbFetch');
 const articleHandler = require('./requestHandlers/article-handler.js');
@@ -42,6 +42,7 @@ app.use( router.routes() );
 app.use(function* index() {
   yield send(this, '/client/index.html');
 });
+
 module.exports = app;
 
 

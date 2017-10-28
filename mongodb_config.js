c94f33512a5692b91c975dce2528b254ea3b7f99
Kenneth Kang <kjkang11@gmail.com>
2017-10-28 09:51:22 -0500

    altering mongodb path back to mlab

--- mongodb_config.js
+++ mongodb_config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost/app';
-// const mongoURI = 'mongodb://newnews:newnews@ds235785.mlab.com:35785/newnews';
+// const mongoURI = 'mongodb://localhost/app';
+const mongoURI = 'mongodb://newnews:newnews@ds235785.mlab.com:35785/newnews';
 mongoose.connect(mongoURI, { useMongoClient: true });
 var mongodb = mongoose.connection;
 mongodb.once('open', function(){

858a6dbef6c35741a59d244e7e7783f858494521
Kenneth Kang <kjkang11@gmail.com>
2017-10-30 17:01:58 -0500

    models.findOneOrCreate modify, retrieveArticles, retrieveByKey, retrieveByConcept, retrieveHeadlines, newsFetcher nestedPromise.all resolve

--- requestHandlers/key-handler.js
+++ requestHandlers/key-handler.js
@@ -1,5 +1,10 @@
 const Key = require('../models/Key.js');
 
-exports.retrieveKeys = () => {
-
+exports.retrieveKeys = async (ctx, next) => {
+  await Key.find({crawl_date: {$gt:yesterday}}) //.where('crawl_date').gt(yesterday)
+  .then( (rows) => {
+    ctx.body = rows;
+  }).catch( (error) => {
+    console.log('inside of key-handler.js retrieveKeys fail', error);
+  })
 }
--- watson/newsFetcher.js
+++ watson/newsFetcher.js
@@ -9,7 +9,6 @@ const Key = require('../models/Key.js');
 const {url, username, password, version} = require('../config/config.js');
 const Continents = require('./Continents.js');
 
-
 /*// node-schedule time format
 'second, minute, hour, dayOfMonth, month, dayOfWeek'*/
 const fetchTime = '0 0 5 * * *'; //every day at 6am
@@ -44,72 +43,64 @@ for(let continentName in Continents){
     // const data = require('./top20_unfiltered.js');
     return Promise.all( data.aggregations[0].aggregations[0].aggregations[0].results.map( ({key, matching_results, aggregations}) => {
 
-      var aggregateByKey = {
-        continent: continentName,
-        key: key,
-        matching_results: matching_results,
-        query_date: today,
-        articles: Promise.all( aggregations[0].hits.hits.map( ({id, score, title, country, crawl_date, url, host, text, main_image_url, enriched_text}) => {
-          return Article.findOneAndUpdate({id:id}, {
-            id: id,
-            key: key,
-            score: score,
-            title: title,
-            country: country,
-            crawl_date: crawl_date,
-            url: url,
-            host: host,
-            text: text,
-            main_image_url: main_image_url,
-            sentiment_score: enriched_text.sentiment.document.score,
-            concepts: enriched_text.concepts,
-            category: enriched_text.categories[0]? enriched_text.categories[0].label : enriched_text.categories[0]
-          }, {upsert:true})
-        }) )
-      }
+      return Promise.all( aggregations[0].hits.hits.map( ({id, score, title, country, crawl_date, url, host, text, main_image_url, enriched_text}) => {
 
-      return Key.findOneAndUpdate({ continent: continentName,
-        key: key,
-        query_date: today }, aggregateByKey, {upsert: true});
+        return Article.findOneOrCreate({id:id}, {
+          id: id,
+          key: key.toLowerCase(),
+          score: score,
+          title: title,
+          country: country,
+          crawl_date: crawl_date,
+          url: url,
+          host: host,
+          text: text,
+          main_image_url: main_image_url,
+          sentiment_score: enriched_text.sentiment.document.score,
+          concepts: enriched_text.concepts,
+          category: enriched_text.categories[0]? enriched_text.categories[0].label : enriched_text.categories[0]
+        })
+
+      }) ).then ( (articles) => { //end of second promise all
+        return articles.filter( ({_id}) => {
+          return _id !== undefined;
+        }).map( ({_id}) => _id );
+
       })
-    )
+      .then((arrayOfIDs) => {
+        var aggregateByKey = {
+          continent: continentName,
+          key: key.toLowerCase(),
+          matching_results: matching_results,
+          query_date: today,
+          article_ids: arrayOfIDs
+        }
 
-  }).then( (rows) => {
-    console.log(`SUCCESS: ${rows.length} concepts saved`)
+        console.log('Aggregation by Key: ', aggregateByKey)
+
+        var query = {
+          continent: continentName,
+          key: key.toLowerCase(),
+          query_date: today
+        }
+
+        console.log('match statement : ', query)
+        return Key.findOneOrCreate(query, aggregateByKey);
+      })
+      .catch(err => {
+        console.log('catching error: ' , err)
+      })
+
+    }) ) //end of first promise all
+
+  }).then( (Keys) => {
+    console.log(`SUCCESS: ${Keys.length} keys saved`)
 
   }).catch( (error) => {
     console.log(error)
   });
 
-//       // .catch( (error) => {
-//       //   console.log('ARTICLES INSERTION FAIL: ',error)
-//       //
-//       // })
-//       .then( (article_ids) => {
-//         // console.log('article_ids = ', article_ids);
-//         let key = {
-//           key: key,
-//           matching_results: matching_results,
-//           continent: continentName,
-//           query_date: today,
-//           article_ids: article_ids
-//         };
-//
-//         return Key.findOneAndUpdate({key: key, continent: continent, query_date:query_date}, key, {upsert:true});
-//
-//       }).then( (keySaved) => {
-//         console.log('KEY SAVED: ', keySaved._id);
-//
-//       }).catch( (error) => {
-//         console.log("KEY INSERTION FAIL: ",error);
-//
-//       });
-//     })
-//
-//
-//   })
-
-};
+}
 
 
 // saving file
@@ -122,27 +113,6 @@ for(let continentName in Continents){
 //     console.log('FILE OPEN FAIL',err);
 //   }
 // })
-
-// static files to mongoDB
-// const continents = ['Africa', 'Asia', 'Europe', 'NorthAmerica', 'Oceania', 'SouthAmerica'];
-
-//
-// continents.map( (continentName) => {
-//   var articles = require(`./${continentName}_top20_filtered.js`);
-//
-//   Promise.all( articles.results.map( (article) => {
-//     return Article.findOneAndUpdate({id: article.id}, article, {upsert:true})
-//     // let newArticle = new Article(article);
-//     // return newArticle.save()
-//     // .catch( (error) => {
-//     //   console.log(error)
-//     //   });
-//     // })
-//     })
-//   );
-//
-// });
-
 
 // const cronRule = new Schedule.RecurrenceRule();
 // cronRule.hour = 5;

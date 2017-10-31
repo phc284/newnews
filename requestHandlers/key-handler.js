664e7538465ccb16507cb944a13c89b89109b5aa
Kenneth Kang <kjkang11@gmail.com>
2017-10-31 13:47:01 -0500

    adding toparticles

--- requestHandlers/key-handler.js
+++ requestHandlers/key-handler.js
@@ -1,13 +1,18 @@
 const Key = require('../models/Key.js');
+const Article = require('../models/Article.js');
+
+let date = new Date();
+const yesterday = [date.getFullYear(), date.getMonth()+1, date.getDate()-1].join('-');
 
 let date = new Date();
 const yesterday = [date.getFullYear(), date.getMonth()+1, date.getDate()-1].join('-');
 
 exports.retrieveKeys = async (ctx, next) => {
-  await Key.find({query_date: {$gt:yesterday}}) //.where('crawl_date').gt(yesterday)
-  .then( (rows) => {
-  	console.log('rows: ', rows);
-    ctx.body = rows;
+  await Key.find({query_date: {$gt:yesterday}}).then( (keyConcepts) => {
+    var topArticles = keyConcepts.map(({article_ids}) => {
+      return Article.find({id: article_ids[0]});
+    });
+    ctx.body = {key: keyConcepts, topArticles: topArticles};
   }).catch( (error) => {
     console.log('inside of key-handler.js retrieveKeys fail', error);
   })

72437d6f3d5ed5dd77f931295cebd8019fce7cf5
Kenneth Kang <kjkang11@gmail.com>
2017-11-01 14:53:10 -0500

    /articles/concept endpoint time optimization && /keys now serves [keys] and [topArticles]

--- requestHandlers/key-handler.js
+++ requestHandlers/key-handler.js
@@ -6,21 +6,17 @@ date.setDate(date.getDate()-1);
 const yesterday = [date.getFullYear(), date.getMonth()+1, date.getDate()].join('-');
 
 exports.retrieveKeys = async (ctx, next) => {
-  await Key.find({query_date: {$gt:yesterday}}).then( (keyConcepts) => {
-    ctx.body = keyConcepts;
+  var keysSave = [];
+  await Key.find({ query_date: {$gt:yesterday} }).then( (keyConcepts) => {
+    keysSave = keyConcepts;
+    return Promise.all( keyConcepts.map( ({article_ids}) => {
+      return Article.find({_id:article_ids[0]})
+    }) )
+
+  }).then( (topArticles) => {
+    ctx.body = {
+      keys: keysSave,
+      topArticles: topArticles
+    }
   })
 }
-//
-//     return Promise.all( keyConcepts.map( ({article_ids}) => {
-//       return Article.find({_id:article_ids[0]})
-//     }) );
-//
-//   }).then((keyObj) => {
-//   //   ctx.body = keyObj;
-//
-//   })//.then( ({  }) => {
-//   //
-//   // }).catch( (error) => {
-//   //   console.log('inside of key-handler.js retrieveKeys fail', error);
-//   // })
-// }

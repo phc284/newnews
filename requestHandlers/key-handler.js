35dfc362c483391f0e5bd45cf8652c82003a3c94
Kenneth Kang <kjkang11@gmail.com>
2017-11-01 11:38:51 -0500

    key-handler temp fix

--- requestHandlers/key-handler.js
+++ requestHandlers/key-handler.js
@@ -6,17 +6,21 @@ date.setDate(date.getDate()-1);
 const yesterday = [date.getFullYear(), date.getMonth()+1, date.getDate()].join('-');
 
 exports.retrieveKeys = async (ctx, next) => {
-  await Key.find({query_date: {$gt:yesterday}}).populate('article_ids').then( (keyConcepts) => {
-    return Promise.all( keyConcepts.map( ({article_ids}) => {
-      return Article.find({_id:article_ids[0]})
-    }) );
-  //
-  // }).then((keyObj) => {
-  //   ctx.body = keyObj;
-
-  })//.then( ({  }) => {
-  //
-  // }).catch( (error) => {
-  //   console.log('inside of key-handler.js retrieveKeys fail', error);
-  // })
+  await Key.find({query_date: {$gt:yesterday}}).then( (keyConcepts) => {
+    ctx.body = keyConcepts;
+  })
 }
+//
+//     return Promise.all( keyConcepts.map( ({article_ids}) => {
+//       return Article.find({_id:article_ids[0]})
+//     }) );
+//
+//   }).then((keyObj) => {
+//   //   ctx.body = keyObj;
+//
+//   })//.then( ({  }) => {
+//   //
+//   // }).catch( (error) => {
+//   //   console.log('inside of key-handler.js retrieveKeys fail', error);
+//   // })
+// }
